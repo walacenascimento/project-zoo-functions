ecb7d8573589560a45edaca3a126d01be3d501a7
Walace Nascimento <wlacens@hotmail.com>
2021-06-30 16:14:27 -0300

    Resolvendo problema de lint

--- src/zoo.js
+++ src/zoo.js
@@ -28,7 +28,7 @@ function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
 
-function countAnimals(species) {
+function countAnimals() {
   // seu código aqui
 }
 
